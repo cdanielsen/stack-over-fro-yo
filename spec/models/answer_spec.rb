1c5ac2a30c4dd2d33a440842860a502a16add707
Dustin Lewis & Christian Danielsen <student@epicodus.com>
2014-09-18 11:21:43 -0700

    complete Vote, Answer models.

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,6 @@
+describe Answer do
+
+  it { should validate_presence_of :content}
+  it { should belong_to :question }
+
+end
---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,6 @@
+describe Vote do
+
+  it { should belong_to :user }
+  it { should belong_to :answer }
+
+end
